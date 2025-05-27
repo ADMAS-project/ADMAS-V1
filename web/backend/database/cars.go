539fcd6ab6e1abddfb863f8dfcbb85d8d0a12a29
canitey <mohamedmed01345@gmail.com>
2025-05-27 18:46:14 +0300

    modified the database actors to add and retrieve more data
    car.color and car.model for addition and retreival

--- web/backend/database/cars.go
+++ web/backend/database/cars.go
@@ -6,8 +6,8 @@ import (
 )
 
 func AddCar(d *sql.DB, carInfo models.CarInfo) (error) {
-	stmt := `INSERT INTO cars(uuid, longitude, latitude, driver_status, served) VALUES ($1, $2, $3, $4, false)`
-	_, err := d.Exec(stmt, carInfo.UUID, carInfo.Longitude, carInfo.Latitude, carInfo.DriverStatus)
+	stmt := `INSERT INTO cars(uuid, longitude, latitude, driver_status, color, model, served) VALUES ($1, $2, $3, $4, $5, $6, false)`
+	_, err := d.Exec(stmt, carInfo.UUID, carInfo.Longitude, carInfo.Latitude, carInfo.DriverStatus, carInfo.Color, carInfo.Model)
 	if err != nil {
 		return err
 	}
@@ -18,8 +18,8 @@ func AddCar(d *sql.DB, carInfo models.CarInfo) (error) {
 // this function gets the cars info from database
 func GetCar(d *sql.DB, carUUID string) (*models.CarInfo, error) {
 	var carinfo models.CarInfo
-	stmt := `SELECT longitude, latitude, driver_status FROM cars WHERE uuid=$1`
-	if err := d.QueryRow(stmt, carUUID).Scan(&carinfo.Longitude, &carinfo.Latitude, &carinfo.DriverStatus); err != nil {
+	stmt := `SELECT longitude, latitude, driver_status, color, model FROM cars WHERE uuid=$1`
+	if err := d.QueryRow(stmt, carUUID).Scan(&carinfo.Longitude, &carinfo.Latitude, &carinfo.DriverStatus, &carinfo.Color, &carinfo.Model); err != nil {
 		return nil, err
 	}
 
