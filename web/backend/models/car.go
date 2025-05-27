d46ad0a45793883768bfe29202d28f46288de9bf
canitey <mohamedmed01345@gmail.com>
2025-05-27 18:41:27 +0300

    changed the car model to more explicit one

--- web/backend/models/car.go
+++ web/backend/models/car.go
@@ -5,4 +5,6 @@ type CarInfo struct {
 	Longitude float64 `json:"longitude"`
 	Latitude float64 `json:"latitude"`
 	DriverStatus string `json:"driver_status"`
+	Model string `json:"model"`
+	Color string `json:"color"`
 }
